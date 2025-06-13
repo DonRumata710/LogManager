06621ac1d25dccd331cad520588ee26a8cf5c41f
DonRumata710 <donrumata710@yandex.com>
2025-06-13 21:37:16 +0400

    fix proxy model update

--- src/LogFilterModel.cpp
+++ src/LogFilterModel.cpp
@@ -4,7 +4,9 @@
 
 
 LogFilterModel::LogFilterModel(QObject *parent) : QSortFilterProxyModel(parent)
-{}
+{
+    setDynamicSortFilter(true);
+}
 
 void LogFilterModel::setFilterWildcard(int column, const QString& pattern)
 {
@@ -22,4 +24,21 @@ void LogFilterModel::setFilterRegularExpression(int column, const QString& patte
     else
         columnFilters[column] = QRegularExpression(pattern);
     invalidateFilter();
+}
+
+bool LogFilterModel::filterAcceptsRow(int source_row, const QModelIndex& source_parent) const
+{
+    if (columnFilters.empty())
+        return true;
+
+    for (auto& filter : columnFilters)
+    {
+        int column = filter.first;
+        const QRegularExpression& rx = filter.second;
+        QModelIndex idx = sourceModel()->index(source_row, column, source_parent);
+        QString text = sourceModel()->data(idx, filterRole()).toString();
+        if (!rx.pattern().isEmpty() && !rx.match(text).hasMatch())
+            return false;
+    }
+    return true;
 }
