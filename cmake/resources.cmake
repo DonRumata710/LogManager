0e7b6e89ffed230515ec2a98e4ffe6a266d21298
DonRumata710 <donrumata710@yandex.com>
2025-08-25 16:24:41 +0400

    update resource file

--- cmake/resources.cmake
+++ cmake/resources.cmake
@@ -14,15 +14,16 @@ endfunction()
 function(generate_qrc_file resources relDir aliasPrefix resFile)
     set(content ""  )
     list(APPEND content "<RCC>")
-    list(APPEND content "<qresource>")
+    list(APPEND content "    <qresource prefix=\"/\">")
 
     foreach(resource IN LISTS resources)
         file(RELATIVE_PATH rel "${relDir}" ${resource})
-        list(APPEND content "<file alias=\"${aliasPrefix}${rel}\">${resource}</file>")
+        list(APPEND content "        <file alias=\"${aliasPrefix}${rel}\">${rel}</file>")
     endforeach()
 
-    list(APPEND content "</qresource>")
+    list(APPEND content "    </qresource>")
     list(APPEND content "</RCC>")
+    list(APPEND content "")
 
     string(REGEX REPLACE ";" "\n" content "${content}")
 
